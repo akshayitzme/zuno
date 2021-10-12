67308b4b582ba021d93449b307b759258b19ae49
shania-maria <shaniamaria129@gmail.com>
2021-10-12 16:33:09 +0530

    add: rounded to input tag

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -18,7 +18,7 @@ function SearchForm() {
 
     return (
         <form onSubmit={formik.handleSubmit}>
-            <input name="username" onChange={formik.handleChange} value={formik.values.username} className="h-12 p-2 text-black" type="search" placehoder="github username" />
+            <input name="username" onChange={formik.handleChange} value={formik.values.username} className="h-12 p-2 text-black rounded" type="search" placehoder="github username" />
             <button className="bg-white" type="submit">Submit</button>
         </form>
     )
